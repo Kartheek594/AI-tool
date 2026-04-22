aa06f241909680d9e47f8ab9a3731fa8038047f8
Kartheek594 <pathlavatharjun5485@gmail.com>
2026-04-23 00:15:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const options = document.querySelectorAll(".option");
+const question = document.getElementById("question").innerText;
+
+options.forEach(option => {
+  option.addEventListener("mouseenter", async () => {
+
+    const response = await fetch("http://127.0.0.1:5000/check", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        question: question,
+        option: option.innerText
+      })
+    });
+
+    const result = await response.json();
+
+    if (result.status === "correct") {
+      option.style.backgroundColor = "green";
+    } else {
+      option.style.backgroundColor = "red";
+    }
+  });
+});
